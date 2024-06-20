e9a681308db56d820b7e268d363e93cc1cf9e873
sorgazb <sorgazb@gmail.com>
2024-06-20 15:12:54 +0200

    comienzo estilos

--- js/app.js
+++ js/app.js
@@ -65,7 +65,7 @@ function resolverOperacion(){
             resultado=Math.pow(operando1,operando2);
             break;
     }
-    operando1=resultado;
+    operando1="Hello World!";
     actualizarPantalla();
     operando2=parseFloat(operando1);
     operando1=0;
