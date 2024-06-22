1f65a751970fcec98059a5c7e7420c6e6cbb467e
sorgazb <sorgazb@gmail.com>
2024-06-22 15:41:25 +0200

    Fin Calculadora JS

--- js/app.js
+++ js/app.js
@@ -4,16 +4,26 @@ let operando2=0;
 let operando2Aux=0;
 let operacion;
 let resultado=0;
+let cadenaOperacion=0;
 
 function actualizarPantalla(){
-    document.getElementById('valores').value=operando1;
+    document.getElementById('valores').value=cadenaOperacion;
 }
 
 function introducirNumero(numero){
-    if(operando1==0 && operando1!=='0.'){
+    if(operando1==0 && operando1!=='0.' && cadenaOperacion==0){
         operando1=numero;
-    }else{
+        cadenaOperacion=operando1;
+    }else if(cadenaOperacion!=0 && operando1==0){
+        operando1=numero;
+        cadenaOperacion+=operando1;
+    }else if(cadenaOperacion!=0 && operando1!=0){
         operando1+=numero;
+        cadenaOperacion=operando1;
+    }
+    else{
+        operando1+=numero;
+        cadenaOperacion+=operando1;
     }
     actualizarPantalla();
 }
@@ -21,15 +31,19 @@ function introducirNumero(numero){
 function introducirComa(){
     if(operando1==0){
         operando1='0.';
+        cadenaOperacion=operando1;
     }else{
         operando1+='.';
+        cadenaOperacion+=operando1;
     }
+    
     actualizarPantalla();
 }
 
 function resetearPantalla(){
     operando1=0;
     operando2=0;
+    cadenaOperacion=0;
     actualizarPantalla();
 }
 
@@ -39,15 +53,17 @@ function realizarOperacion(valor){
     }
     operando1Aux=parseFloat(operando1);
     operacion=valor;
+    cadenaOperacion+=operacion;
     actualizarPantalla();
-    operando1=0;
-    
+    operando1=0; 
 }
 
 function resolverOperacion(){
     operando2Aux=parseFloat(operando1);
     operando2=operando2Aux;
     operando1=operando1Aux;
+    cadenaOperacion+=operando2;
+    actualizarPantalla();
     switch (operacion) {
         case '+':
             resultado=(operando1+operando2);
@@ -65,7 +81,7 @@ function resolverOperacion(){
             resultado=Math.pow(operando1,operando2);
             break;
     }
-    operando1=resultado;
+    cadenaOperacion=resultado;
     actualizarPantalla();
     operando2=parseFloat(operando1);
     operando1=0;
